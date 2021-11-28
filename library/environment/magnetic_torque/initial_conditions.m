ee1a642254ec6d122c892958a012037ace4c2242
whitehole <danieleagamennone@gmail.com>
2021-11-28 21:13:06 +0100

    fixed some bugs in magnetic_torque

--- library/environment/magnetic_torque/initial_conditions.m
+++ library/environment/magnetic_torque/initial_conditions.m
@@ -4,3 +4,9 @@ we = 7.2916e-05;  % earth rotation speed
 t0 = 0;           % init time
 theta_g0 = 0;     % theta greenwich at t0
 init_mjd2000 = 0; % initial mjd2000
+
+igrfSg = fopen('igrfSg.txt');
+igrfSh = fopen('igrfSh.txt');
+
+g_mat = cell2mat(textscan(igrfSg, '%f %f %f %f'));
+h_mat = cell2mat(textscan(igrfSh, '%f %f %f %f'));
