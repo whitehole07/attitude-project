6b8a96ae9e2851f8a5738bcdbcf983cb25376bbe
whitehole <danieleagamennone@gmail.com>
2021-11-28 18:42:18 +0100

    implemented magnetic torque

--- lab2.m
+++
@@ -1,9 +0,0 @@
-clc;
-close all;
-clear;
-
-g = 9.81;
-l = 2;
-g_l = g/l;
-theta0 = 0 + 5*pi/180;
-theta_dot0 = 0;
---
+++ library/environment/magnetic_torque/initial_conditions.m
@@ -0,0 +1,6 @@
+clear we t0 theta_g0 init_mjd2000
+
+we = 7.2916e-05;  % earth rotation speed
+t0 = 0;           % init time
+theta_g0 = 0;     % theta greenwich at t0
+init_mjd2000 = 0; % initial mjd2000
