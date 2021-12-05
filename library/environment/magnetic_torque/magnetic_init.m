070c52049b08a5680c860e3ff4e308d4c565f584
whitehole <danieleagamennone@gmail.com>
2021-12-05 10:24:02 +0100

    update commit

--- library/environment/magnetic_torque/magnetic_init.m
+++ library/environment/magnetic_torque/magnetic_init.m
@@ -3,7 +3,7 @@ clear we t0 theta_g0 init_mjd2000
 we = 7.2916e-05;  % earth rotation speed
 t0 = 0;           % init time
 theta_g0 = 0;     % theta greenwich at t0
-init_mjd2000 = 0; % initial mjd2000
+init_mjd2000 = 0; % init mjd2000
 
 igrfSg = fopen('igrfSg.txt');
 igrfSh = fopen('igrfSh.txt');
